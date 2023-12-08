4a20d1731e0ed1491cc54afca18b3fb220a1a4d8
Vatican Memes <feehanharry@gmail.com>
2023-12-08 17:16:49 +1100

    started over with the website, nicely formatted atm

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 console.log("hello world lmao")
 
 const myLibrary = [];
@@ -21,9 +22,30 @@ const book = new Book("The Hobbit", "JRR Tolkein", "320", true);
 console.log(book.getInfo());
 
 function addBookToLibrary() {
-    myLibrary.push(book)
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read_status = document.getElementById("read_status").value;
 }
 
 while(true) {
-    // blah blah
+    break;
 }
+*/
+
+// Bad code lmao - fucked the whole thing up. The new code shall be better
+
+class Book {
+    constructor(title, author, pages, read_status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read_status = read_status;
+    }
+
+    getInfo() {
+        const read_statement = (this.read_status == true) ? "read": "not read yet";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${read_statement}`;
+    }
+}
+
